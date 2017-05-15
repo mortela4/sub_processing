4bb9a22d2ac7bb1d86d986052f8e1f17bed67109
mortela4 <morten.larsen@7sense.no>
2017-05-15 21:08:07 +0200

    Added project to solution.

---
+++ sub_process_ex/target_bl_commands/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace target_bl_commands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var UUT = new rom_bl_commands();
+
+            string res = ( UUT.BLhostErase() ) ? "PASS" : "FAIL";
+
+            Console.WriteLine("Command result: " + res);
+        }
+    }
+}
---
+++ sub_process_ex/target_bl_commands/rom_bl_commands.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace target_bl_commands
+{
+
+    class rom_bl_commands
+    {
+        public const string BLHOST_CMD_PREFIX = @"\blhost_files\blhost.exe";
+        public const string TARGET_UID_RESPONSE = @"Unique Device ID = ";       // 'Get UID' cmd pattern
+
+        public static string targetUID = string.Empty;
+
+        public rom_bl_commands()
+        {
+            // pass
+        }
+
+        // Generic command-runner : PRIVATE
+        private static Tuple<bool,string> RunBLhostCmd(string cmd)
+        {
+            string patternLine = string.Empty;
+            string fullPathCmd = Directory.GetCurrentDirectory() + BLHOST_CMD_PREFIX;
+            Tuple<bool, string> retVal;
+
+            Process process = new Process();
+            process.StartInfo.FileName = fullPathCmd;
+            process.StartInfo.Arguments = " -u -- " + cmd;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                if (e.Data != null)
+                {
+                    // Debug:
+                    Console.WriteLine("Got line: " + e.Data);
+                    // Process Line:
+                    if ( e.Data.StartsWith(TARGET_UID_RESPONSE) )
+                    {
+                        var subStrings = e.Data.Split( new char[] { '=' } );
+                        targetUID = subStrings[1];
+                    }
+                }
+            });
+            process.Start();
+            process.BeginOutputReadLine();   // Line-buffered
+            process.WaitForExit();           // *SKAL* være safe - men OBS: chars KAN ankomme senere!!
+            if (process.ExitCode == 0)
+                retVal = new Tuple<bool, string>(true, patternLine);
+            else
+                retVal = new Tuple<bool, string>(false, string.Empty);
+
+            // Etter 'Close()' vil process-pbjekt være =null.
+            process.Close();
+
+            return retVal;
+        }
+
+        // Target-specific commands : PUBLIC
+        public bool BLhostErase()
+        {
+            bool status = false;
+            string result;
+
+            string cmd = "flash-erase-all-unsecure";
+
+            var ret = RunBLhostCmd(cmd);
+            status = ret.Item1;
+            result = ret.Item2;
+
+            return status;
+        }
+
+        public bool BLhostProgram(string binFile)
+        {
+            bool status = false;
+
+            return status;
+        }
+
+        public bool BLhostGetUID()
+        {
+            bool status = false;
+
+            return status;
+        }
+
+        public bool BLhostWriteSensorID(string sensorID)
+        {
+            bool status = false;
+
+            return status;
+        }
+
+    }
+
+}
